6985e3db49fa7f3e46114f4fdf952b49d46fa61a
Henning Jensen <henning@arkitektum.no>
2014-10-30 10:43:04 +0100

    Create project with GeoNetworkUtil.

---
+++ Kartverket.Geonorge.Utilities.Tests/GeoNetworkUtilTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Kartverket.Geonorge.Utilities.Tests
+{
+    class GeoNetworkUtilTest
+    {
+        private const string BaseUrl = "http://www.geonorge.no/geonetwork/";
+        private const string Uuid = "4403b8b6-6c10-4570-b402-8633e03a606e";
+
+        [Test]
+        public void ShouldReturnViewUrl()
+        {
+            var util = new GeoNetworkUtil(BaseUrl);
+            string viewUrl = util.GetViewUrl(Uuid);
+
+            viewUrl.Should().Be(BaseUrl + "?uuid=" + Uuid);
+        }
+
+        [Test]
+        public void ShouldReturnXmlDownloadUrl()
+        {
+            var util = new GeoNetworkUtil(BaseUrl);
+            string xmlDownloadUrl = util.GetXmlDownloadUrl(Uuid);
+
+            xmlDownloadUrl.Should().Be(BaseUrl + "srv/nor/xml_iso19139?uuid=" + Uuid);
+        }
+
+    }
+}
---
+++ Kartverket.Geonorge.Utilities/GeoNetworkUtil.cs
@@ -0,0 +1,23 @@
+﻿namespace Kartverket.Geonorge.Utilities
+{
+    public class GeoNetworkUtil
+    {
+        private readonly string _baseUrl;
+
+        public GeoNetworkUtil(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+        }
+
+        public string GetViewUrl(string uuid)
+        {
+            return string.Format("{0}?uuid={1}", _baseUrl, uuid);
+        }
+
+        public string GetXmlDownloadUrl(string uuid)
+        {
+            return string.Format("{0}srv/nor/xml_iso19139?uuid={1}", _baseUrl, uuid);
+        }
+
+    }
+}
