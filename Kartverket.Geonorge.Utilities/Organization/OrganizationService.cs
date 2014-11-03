5ad800f54b9e0db171bdc647009332a4302a595e
Henning Jensen <henning@arkitektum.no>
2014-11-03 10:21:01 +0100

    Add logging.

--- Kartverket.Geonorge.Utilities/Organization/OrganizationService.cs
+++ Kartverket.Geonorge.Utilities/Organization/OrganizationService.cs
@@ -2,11 +2,14 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Kartverket.Geonorge.Utilities.Logging;
 
 namespace Kartverket.Geonorge.Utilities.Organization
 {
     public class OrganizationService : IOrganizationService
     {
+        private static readonly ILog Logger = LogProvider.For<OrganizationService>(); 
+
         private readonly string _registryUrl;
         private readonly IHttpClientFactory _factory;
 
@@ -18,6 +21,7 @@ namespace Kartverket.Geonorge.Utilities.Organization
 
         public async Task<Organization> GetOrganizationByName(string name)
         {
+            Logger.Info(string.Format("Looking up organization by name: {0}", name));
             HttpClient client = _factory.GetHttpClient();
 
             client.BaseAddress = new Uri(_registryUrl);
@@ -28,8 +32,10 @@ namespace Kartverket.Geonorge.Utilities.Organization
             if (response.IsSuccessStatusCode)
             {
                 Organization organization = await response.Content.ReadAsAsync<Organization>().ConfigureAwait(false);
+                Logger.Info(string.Format("Organization [{0}] found. [Number={1}], [LogoUrl={2}]", organization.Name, organization.Number, organization.LogoUrl));
                 return organization;
             }
+            Logger.Info(string.Format("Organization [{0}] not found. Http response code: {1}", name, response.StatusCode));
             return null;
         }
     }
