2547d2c1c4fecb62901ddbde371bae076a84af01
Henning Jensen <henning@arkitektum.no>
2014-11-06 10:56:53 +0100

    Add method GetThumbnailUrl to GeoNetworkUtil.

--- Kartverket.Geonorge.Utilities.Tests/GeoNetworkUtilTest.cs
+++ Kartverket.Geonorge.Utilities.Tests/GeoNetworkUtilTest.cs
@@ -26,5 +26,42 @@ namespace Kartverket.Geonorge.Utilities.Tests
             xmlDownloadUrl.Should().Be(BaseUrl + "srv/nor/xml_iso19139?uuid=" + Uuid);
         }
 
+        [Test]
+        public void ShouldReturnOriginalThumbnailUrlWhenNull()
+        {
+            var util = new GeoNetworkUtil(BaseUrl);
+            string thumbnailUrl = util.GetThumbnailUrl(Uuid, null);
+
+            thumbnailUrl.Should().BeNull();
+        }
+
+        [Test]
+        public void ShouldReturnOriginalThumbnailUrlWhenEmpty()
+        {
+            var util = new GeoNetworkUtil(BaseUrl);
+            string thumbnailUrl = util.GetThumbnailUrl(Uuid, "");
+
+            thumbnailUrl.Should().Be("");
+        }
+
+        [Test]
+        public void ShouldReturnOriginalThumbnailUrlWhenUrlStartsWithHttp()
+        {
+            var util = new GeoNetworkUtil(BaseUrl);
+            string exampleImage = "http://example.com/myimage.png";
+            string thumbnailUrl = util.GetThumbnailUrl(Uuid, exampleImage);
+
+            thumbnailUrl.Should().Be(exampleImage);
+        }
+
+        [Test]
+        public void ShouldReturnThumbnailUrlFromGeoNetworkWhenThumbnailOnlyContainsFilename()
+        {
+            var util = new GeoNetworkUtil(BaseUrl);
+            string exampleImage = "myimage.png";
+            string thumbnailUrl = util.GetThumbnailUrl(Uuid, exampleImage);
+
+            thumbnailUrl.Should().Be(BaseUrl + "srv/nor/resources.get?uuid=" + Uuid + "&access=public&fname=" + exampleImage);
+        }
     }
 }
--- Kartverket.Geonorge.Utilities/GeoNetworkUtil.cs
+++ Kartverket.Geonorge.Utilities/GeoNetworkUtil.cs
@@ -19,5 +19,13 @@
             return string.Format("{0}srv/nor/xml_iso19139?uuid={1}", _baseUrl, uuid);
         }
 
+        public string GetThumbnailUrl(string uuid, string thumbnail)
+        {
+            if (!string.IsNullOrWhiteSpace(thumbnail) && !thumbnail.StartsWith("http"))
+            {
+                 return string.Format("{0}srv/nor/resources.get?uuid={1}&access=public&fname={2}", _baseUrl, uuid, thumbnail);
+            }
+            return thumbnail;
+        }
     }
 }
