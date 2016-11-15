b910c0a201d79bff4d4ef6991c54d9c805c1f88a
Henning Jensen <henning.jensen@gmail.com>
2016-11-15 15:54:56 +0100

    Add SecurityClaim utility

---
+++ Kartverket.Geonorge.Utilities/SecurityClaim.cs
@@ -0,0 +1,31 @@
+namespace Kartverket.Geonorge.Utilities
+{
+    public static class SecurityClaim
+    {
+        private static string GetUsername()
+        {
+            return GetSecurityClaim("username");
+        }
+
+        private static string GetSecurityClaim(string type)
+        {
+            string result = null;
+            foreach (var claim in System.Security.Claims.ClaimsPrincipal.Current.Claims)
+            {
+                if (claim.Type == type && !string.IsNullOrWhiteSpace(claim.Value))
+                {
+                    result = claim.Value;
+                    break;
+                }
+            }
+
+            // bad hack, must fix BAAT
+            if (!string.IsNullOrWhiteSpace(result) && type.Equals("organization") && result.Equals("Statens kartverk"))
+            {
+                result = "Kartverket";
+            }
+
+            return result;
+        }
+    }
+}
