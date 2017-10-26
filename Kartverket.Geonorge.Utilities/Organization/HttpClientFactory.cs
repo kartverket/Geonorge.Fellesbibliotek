f836779306fbfbd293737159ee8ca878688e7d7f
Dag Olav <dagolav@arkitektum.no>
2017-10-26 08:51:27 +0200

    Reuse HttpClient Details: https://aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/

--- Kartverket.Geonorge.Utilities/Organization/HttpClientFactory.cs
+++ Kartverket.Geonorge.Utilities/Organization/HttpClientFactory.cs
@@ -4,9 +4,11 @@ namespace Kartverket.Geonorge.Utilities.Organization
 {
     public class HttpClientFactory : IHttpClientFactory
     {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
         public HttpClient GetHttpClient()
         {
-            return new HttpClient();
+            return _httpClient;
         }
     }
 }
